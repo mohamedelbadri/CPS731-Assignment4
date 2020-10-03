5550a33d03f6e80dbc5d627c4a69bc7f198ae903
Mo Elbadry <mohdelbadry@gmail.com>
2020-10-03 17:16:17 -0400

    added a recipe page

---
+++ ReyclerView_images/app/src/main/java/com/example/reyclerview_images/foodRecipe.java
@@ -0,0 +1,14 @@
+package com.example.reyclerview_images;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class foodRecipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_recipe);
+    }
+}
